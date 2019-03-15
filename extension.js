d5b611e9f8853da7eff49f57a67211595ee514e5
tomschilling <tom.schilling@hsh-berlin.com>
2019-03-15 19:32:37 +0100

    code style improvements

--- extension.js
+++ extension.js
@@ -1,8 +1,42 @@
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
+
 const vscode = require('vscode')
 
-const insertText = (val) => {
+module.exports = {
+  activate
+}
+
+/**
+ * @param {vscode.ExtensionContext} context
+ */
+function activate (context) {
+  const insertLogStatement = vscode.commands.registerCommand('extension.addFunction', () => {
+    const editor = vscode.window.activeTextEditor
+    if (!editor) { return }
+    const selection = editor.selection
+    const text = editor.document.getText(selection)
+
+    if (text) {
+      vscode.commands.executeCommand('cursorBottom')
+        .then(() => {
+          vscode.commands.executeCommand('editor.action.insertLineAfter')
+            .then(() => {
+              const func = text.replace('(', ' (')
+              const funcToInsert = `\nfunction ${func} {\n\n}`
+              _insertFunc(funcToInsert)
+            })
+        })
+    } else {
+      vscode.window.showInformationMessage('Please select function!')
+    }
+  })
+  context.subscriptions.push(insertLogStatement)
+}
+
+/**
+ * Insert the selected function with a new line at the end of the file
+ * @param {String} val
+ */
+function _insertFunc (val) {
   const editor = vscode.window.activeTextEditor
 
   if (!editor) {
@@ -11,54 +45,9 @@ const insertText = (val) => {
   }
 
   const selection = editor.selection
-
   const range = new vscode.Range(selection.start, selection.end)
 
   editor.edit((editBuilder) => {
     editBuilder.replace(range, val)
   })
-}
-
-// this method is called when your extension is activated
-// your extension is activated the very first time the command is executed
-
-/**
- * @param {vscode.ExtensionContext} context
- */
-function activate (context) {
-  // Use the console to output diagnostic information (console.log) and errors (console.error)
-  // This line of code will only be executed once when your extension is activated
-  console.log('Congratulations, your extension "vscode-js-add-function" is now active!')
-
-  // The command has been defined in the package.json file
-  // Now provide the implementation of the command with  registerCommand
-  // The commandId parameter must match the command field in package.json
-  const insertLogStatement = vscode.commands.registerCommand('extension.addFunction', () => {
-    const editor = vscode.window.activeTextEditor
-    if (!editor) { return }
-
-    const selection = editor.selection
-    const text = editor.document.getText(selection)
-
-    text
-      ? vscode.commands.executeCommand('cursorBottom')
-        .then(() => {
-          vscode.commands.executeCommand('editor.action.insertLineAfter')
-            .then(() => {
-              const logToInsert = `function ${text} {\n\n}`
-              insertText(logToInsert)
-            })
-        })
-      : vscode.window.showInformationMessage('Please highlight function!')
-  })
-  context.subscriptions.push(insertLogStatement)
-}
-exports.activate = activate
-
-// this method is called when your extension is deactivated
-function deactivate () {}
-
-module.exports = {
-  activate,
-  deactivate
 }
